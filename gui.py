a2db07f3fd413eadfa216922a45ebc7e87fa1618
Loai <saadiloai@gmail.com>
2023-09-02 11:09:00 +0300

    Create a static web interface

--- gui.py
+++ gui.py
@@ -1,13 +1,18 @@
 import functions
 import PySimpleGUI as sg
 import time
+import os
+
+if not os.path.exists('todos.txt'):
+    with open('todos.txt', 'w') as file:
+        pass
 
 sg.theme("DarkTeal10")
 
 clock = sg.Text("", key="clock")
 label = sg.Text("Type in a to-do")
 input_box = sg.InputText(tooltip="Enter todo", key="todo")
-add_button = sg.Button("Add")
+add_button = sg.Button("Add", bind_return_key=True)
 list_box = sg.Listbox(values=functions.get_todos(), key="todos",
                       enable_events=True, size=(44, 10))
 edit_button = sg.Button("Edit")
@@ -22,20 +27,26 @@ window = sg.Window("My To-Do App",
                            [exit_button]],
                    font=('Helvetica', 12))
 while True:
-    event, values = window.read(timeout=200) # update every one second
+    event, values = window.read(timeout=200)    # update every one second=1000
     window["clock"].update(value=time.strftime("%b %d, %Y %H:%M:%S"))
+
     match event:
         case "Add":
             todos = functions.get_todos()
+            if values["todo"] == "":
+                continue
             new_todo = values["todo"] + "\n"
             todos.append(new_todo)
             functions.write_todos(todos)
             window["todos"].update(values=todos)
+            window["todo"].update('')
 
         case "Edit":
             try:
+                if values["todo"] == "":
+                    continue
                 todo_to_edit = values["todos"][0]
-                new_todo = values["todo"] # + '\n
+                new_todo = values["todo"]
 
                 todos = functions.get_todos()
                 index = todos.index(todo_to_edit)
